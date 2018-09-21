079f81d01ca2ea1618a755b8b76879354f373b8b
Graham John <graham@brambles.org>
2018-09-21 18:11:47 +0100

    Add project template

---
+++ src/improve/Program.cs
@@ -0,0 +1,13 @@
+﻿using GACJ.Ringing.Methods;
+using System;
+
+namespace improve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
